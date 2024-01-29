c9278456988b9fdb7ace7292502fc284ab554b25
Ishibashi Takuma <t-ishibashi@Alcyone.local>
2024-01-29 12:13:51 +0900

    fix university

--- lib/domain/teacher/models/university.dart
+++ lib/domain/teacher/models/university.dart
@@ -1,13 +1,14 @@
-
-import '../../school/models/school.dart'
+import '../../school/models/school.dart';
 import 'graduated.dart';
 
 class University extends School {
   final EnrollmentStatus _enrollmentStatus;
 
-  University({required final School school, required final EnrollmentStatus enrollmentStatus})
-   : _enrollmentStatus = enrollmentStatus,
-   super(school);
+  EnrollmentStatus get enrollmentStatus => _enrollmentStatus;
 
-
+  University(
+      {required final String school,
+      required final EnrollmentStatus enrollmentStatus})
+      : _enrollmentStatus = enrollmentStatus,
+        super(school);
 }
