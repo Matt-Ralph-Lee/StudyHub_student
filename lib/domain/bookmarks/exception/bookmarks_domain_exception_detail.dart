5f354405dee802b035026add63e1795d2ad60616
Matt523 <matthewralphlee@gmail.com>
2024-02-08 17:46:54 +0900

    fix exception message

--- lib/domain/bookmarks/exception/bookmarks_domain_exception_detail.dart
+++ lib/domain/bookmarks/exception/bookmarks_domain_exception_detail.dart
@@ -2,9 +2,9 @@ import '../../shared/domain_exception_detail.dart';
 import '../models/bookmarks.dart';
 
 enum BookmarksDomainExceptionDetail implements DomainExceptionDetail {
-  invalidQuestionLength('先生の数が多すぎます。${Bookmarks.maxLength}人以内にしてください。'),
-  duplicateQuestion("同じ先生が選択されています。"),
-  questionNotFound("先生が見つかりませんでした。");
+  invalidQuestionLength('質問の数が多すぎます。${Bookmarks.maxLength}個以内にしてください。'),
+  duplicateQuestion("同じ質問が選択されています。"),
+  questionNotFound("質問が見つかりませんでした。");
 
   const BookmarksDomainExceptionDetail(this._message);
 
