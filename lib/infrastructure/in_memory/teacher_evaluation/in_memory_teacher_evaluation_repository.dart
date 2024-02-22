62b7d163014b1366a2e176c4da85a829d1085c69
Matt523 <matthewralphlee@gmail.com>
2024-02-22 23:43:38 +0900

    fixed evaluation based on comments

--- lib/infrastructure/in_memory/teacher_evaluation/in_memory_teacher_evaluation_repository.dart
+++ lib/infrastructure/in_memory/teacher_evaluation/in_memory_teacher_evaluation_repository.dart
@@ -1,14 +1,18 @@
-import 'package:studyhub/domain/teacher_evaluation/models/teacher_evaluation.dart';
-import 'package:studyhub/domain/teacher_evaluation/models/i_teacher_evaluation_repository.dart';
-import 'package:studyhub/domain/teacher/models/teacher_id.dart';
+import '../../../domain/teacher_evaluation/models/teacher_evaluation.dart';
+import '../../../domain/teacher_evaluation/models/i_teacher_evaluation_repository.dart';
+import '../../../domain/teacher/models/teacher_id.dart';
 
 class InMemoryTeacherEvaluationRepository
     implements ITeacherEvaluationRepository {
-  final store = <TeacherId, TeacherEvaluation>{};
+  final store = <TeacherId, List<TeacherEvaluation>>{};
   @override
   Future<void> save(TeacherEvaluation evaluation) async {
     final teacherId = evaluation.to;
-    store[teacherId] = evaluation;
+    if (store[teacherId] == null) {
+      store[teacherId] = [evaluation];
+    } else {
+      store[teacherId]!.add(evaluation);
+    }
   }
 
 // fetch all evaluation from store and convert to evaluation
@@ -16,6 +20,6 @@ class InMemoryTeacherEvaluationRepository
   List<TeacherEvaluation>? getByTeacherId(TeacherId teacherId) {
     final evaluation = store[teacherId];
     if (evaluation == null) return null;
-    return [evaluation];
+    return evaluation;
   }
 }
