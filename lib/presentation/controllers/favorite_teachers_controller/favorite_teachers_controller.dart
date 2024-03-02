967403d545ed9afa728f6b3a538e67b8afac668d
githubkazukazu <115880119+githubkazukazu@users.noreply.github.com>
2024-03-02 13:05:54 +0900

    changed favorite_teacher_controller

--- lib/presentation/controllers/favorite_teachers_controller/favorite_teachers_controller.dart
+++ lib/presentation/controllers/favorite_teachers_controller/favorite_teachers_controller.dart
@@ -1,19 +1,43 @@
 import "package:riverpod_annotation/riverpod_annotation.dart";
+import "package:studyhub/application/shared/session/session.dart";
 
+import "../../../application/di/favorite_teacher/query_service/get_favorite_teacher_query_service_provider.dart";
+import "../../../application/di/session/session_provider.dart";
+import "../../../application/favorite_teachers/application_service/get_favorite_teacher_dto.dart";
 import "../../../application/favorite_teachers/application_service/get_favorite_teacher_use_case.dart";
 
 part "favorite_teachers_controller.g.dart";
 
 @riverpod
 class FavoriteTeacherController extends _$FavoriteTeacherController {
-  @override
-  Future<void> build() async {}
+  /*
+  Future<void> fetchFavoriteTeachers() async {
+    final session = ref.read(sessionDiProvider);
+    if (session == null) {
+      throw Exception('Session is null');
+    }
 
-  void fetchFavoriteTeachers() async {
-    final getFavoriteTeacherUseCase =
-        GetFavoriteTeacherUseCase(session: null, queryService: null);
+    final getFavoriteTeacherUseCase = GetFavoriteTeacherUseCase(
+        session: session,
+        queryService: ref.read(getFavoriteTeacherQueryServiceDiProvider));
+
     state = const AsyncLoading();
     final favoriteTeachers = getFavoriteTeacherUseCase.execute();
-    state = AsyncValue.data(getFavoriteTeacherUseCase);
+    state = AsyncValue.data(favoriteTeachers);
+  }
+  */
+
+  @override
+  Future<List<GetFavoriteTeacherDto>> build() async {
+    final session = ref.watch(sessionDiProvider);
+    if (session == null) {
+      throw Exception('Session is null');
+    }
+
+    final getFavoriteTeacherUseCase = GetFavoriteTeacherUseCase(
+        session: session,
+        queryService: ref.watch(getFavoriteTeacherQueryServiceDiProvider));
+    final favoriteTeachers = getFavoriteTeacherUseCase.execute();
+    return favoriteTeachers;
   }
 }
