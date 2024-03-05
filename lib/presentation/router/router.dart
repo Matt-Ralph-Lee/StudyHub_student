4faec6c6144ec64483d5acc87126aafab6b80838
githubkazukazu <115880119+githubkazukazu@users.noreply.github.com>
2024-03-05 18:26:08 +0900

    router.dartのimport文を相対パスで揃えました

--- lib/presentation/router/router.dart
+++ lib/presentation/router/router.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import "package:riverpod_annotation/riverpod_annotation.dart";
 import "package:go_router/go_router.dart";
 import "package:studyhub/presentation/auth/tmp.dart";
-import "package:studyhub/presentation/pages/notification_page.dart";
 import "package:studyhub/presentation/pages/page1.dart";
 import "package:studyhub/presentation/pages/page2.dart";
 import "package:studyhub/presentation/pages/page3.dart";
-import "package:studyhub/presentation/pages/profile_input_page.dart";
 
 import "../pages/auth_page.dart";
 import "../pages/menu_page.dart";
+import "../pages/notification_page.dart";
+import "../pages/profile_input_page.dart";
 import "../pages/reset_password_page.dart";
 import "../pages/search_teachers_page.dart";
 import '../shared/utils/accessibility.dart';
