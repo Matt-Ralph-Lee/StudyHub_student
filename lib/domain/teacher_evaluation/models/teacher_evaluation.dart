6dc2a4d90276fb0928ca4350483ce2b5c5038822
Matt523 <matthewralphlee@gmail.com>
2024-03-24 20:19:12 +0900

    add createdAt property to evaluation

--- lib/domain/teacher_evaluation/models/teacher_evaluation.dart
+++ lib/domain/teacher_evaluation/models/teacher_evaluation.dart
@@ -8,19 +8,23 @@ class TeacherEvaluation {
   final TeacherId _to;
   final TeacherEvaluationRating _rating;
   final TeacherEvaluationComment _comment;
+  final DateTime _createdAt;
 
   StudentId get from => _from;
   TeacherId get to => _to;
   TeacherEvaluationRating get rating => _rating;
   TeacherEvaluationComment get comment => _comment;
+  DateTime get createdAt => _createdAt;
 
   TeacherEvaluation({
-    required StudentId from,
-    required TeacherId to,
-    required TeacherEvaluationRating rating,
-    required TeacherEvaluationComment comment,
+    required final StudentId from,
+    required final TeacherId to,
+    required final TeacherEvaluationRating rating,
+    required final TeacherEvaluationComment comment,
+    required final DateTime createdAt,
   })  : _from = from,
         _to = to,
         _rating = rating,
-        _comment = comment;
+        _comment = comment,
+        _createdAt = createdAt;
 }
