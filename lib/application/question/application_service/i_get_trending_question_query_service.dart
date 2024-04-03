7ab93b85b7ba042e16f249bc4787916e77300ea9
Matt523 <matthewralphlee@gmail.com>
2024-04-04 00:08:34 +0900

    delete useless trending usecase

--- lib/application/question/application_service/i_get_trending_question_query_service.dart
+++
@@ -1,6 +0,0 @@
-import '../../../domain/shared/subject.dart';
-import '../../shared/application_service/question_card_dto.dart';
-
-abstract class IGetTrendingQuestionQueryService {
-  List<QuestionCardDto> get(final Subject? subject);
-}
